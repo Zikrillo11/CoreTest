53a97f414ac63f61a626d25b9063c894b163297a
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-05-05 15:01:52 +0500

    modellar yozildi

---
+++ CoreTest.Domain/Entites/UserTestSolutions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreTest.Domain.Entites;
+
+[Table("UserTestSolutions")]
+public class UserTestSolution
+{
+    [Column("UserId")]
+    public int UserId { get; set; }
+
+    [Column("TestSolutionId")]
+    public int TestSolutionId { get; set; }
+}
