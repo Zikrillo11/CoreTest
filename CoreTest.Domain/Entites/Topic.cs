632306b218b4f0d14f7dbe6a6bd88caa7d639d1a
Akhadjonov Abdulaziz <akhadjonovabdulaziz33@gmail.com>
2026-05-06 20:53:27 +0300

    models for TestSolution, Option and Topic

---
+++ CoreTest.Domain/Entites/Topic.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreTest.Domain.Entites;
+//Topic -> Id, Text, CategoryId
+public class Topic
+{
+    [Column("id")]
+    public long Id { get; set; }
+
+    [Column("Text")]
+    public string Text { get; set; }
+
+    [Column("CategoryId")]
+    public long CategoryId { get; set; }
+}
