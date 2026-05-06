c39d7ba90e801a7060cc6e8eadbbc265dc9dc0ef
Zikriyojonov <user@WIN-M6UCRNPVE06>
2026-05-06 16:34:08 +0500

    Add Category, Questions, and Test entity classes
    Removed WeatherForecast.cs. Added new entity classes (Category, Questions, Test) in CoreTest.Domain.Entites with table and column mappings for database integration. Note: Typo in 'Descriiption' and 'MinPersantage' property names in Test class.

---
+++ CoreTest.Domain/Entites/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreTest.Domain.Entites;
+
+[Table("Category")]
+public class Category
+{
+    [Column("Id")]
+    public long Id { get; set; }
+    [Column("Text")]
+    public string Text { get; set; }
+}
---
+++ CoreTest.Domain/Entites/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreTest.Domain.Entites;
+
+[Table("Test")]
+public class Test
+{
+    [Column("id")]
+    public long Id { get; set; }
+    [Column("title")]
+    public string Title { get; set; }
+    [Column("description")]
+    public string Descriiption { get; set; }
+    [Column("questions")]
+    public string Questions { get; set; }
+    [Column("user_id")]
+    public long UserId { get; set; }
+    [Column("topic")]
+    public string Topic { get; set; }
+    [Column("minpersantage")]
+    public long MinPersantage { get; set; }
+}
