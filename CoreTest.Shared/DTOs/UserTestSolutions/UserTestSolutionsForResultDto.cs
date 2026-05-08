3eeeb5562c620c73e422d51a2c05d3ab056392c2
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-05-08 17:04:56 +0500

    Dto yozildi

---
+++ CoreTest.Shared/DTOs/UserTestSolutions/UserTestSolutionsForResultDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreTest.Shared.DTOs.UserTestSolutions;
+
+public class UserTestSolutionsForResultDto : BaseDto
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int TestSolutionId { get; set; }
+}
