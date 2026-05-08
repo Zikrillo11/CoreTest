09e5c3c493a5a08f8dbdf548d5732d01460e2465
Akhadjonov Abdulaziz <akhadjonovabdulaziz33@gmail.com>
2026-05-08 05:24:42 +0300

    .........

---
+++ CoreTest.Shared/DTOs/TestSolution/TestSolutionForResultDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreTest.Shared.DTOs.TestSolution;
+
+public class TestSolutionForResultDTO : BaseDto
+{
+    public long Id { get; set; }
+    public long UserTestSolutionId { get; set; }
+    public long TestId { get; set; }
+    public double Score { get; set; }
+    public int QuestionCount { get; set; }
+}
