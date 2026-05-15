29c5c5e21d62065ce24be31bcb4860b8c7260404
Zikriyojonov <user@WIN-M6UCRNPVE06>
2026-05-15 14:26:10 +0500

    Added interfaces

---
+++ CoreTest.Contracts/Interface/Question/IQuestionService.cs
@@ -0,0 +1,15 @@
+﻿using CoreTest.Common.Pagination;
+using CoreTest.Contracts.Criteria.Questions;
+using CoreTest.Shared.DTOs.Questions;
+
+namespace CoreTest.Contracts.Interface.Question
+{
+    public interface IQuestionService
+    {
+        Task<PagedList<QuestionForResultDTO>> GetAllAsync(QuestionsCriteria criteria);
+        Task<QuestionForResultDTO> GetByIdAsync(long id);
+         Task<long> CreateAsync(QuestionForCreateDTO newDTO);
+         Task<bool> UpdateAsync(QuestionForUpdateDTO updateDTO);
+         Task<bool> DeleteAsync(long id);
+    }
+}
