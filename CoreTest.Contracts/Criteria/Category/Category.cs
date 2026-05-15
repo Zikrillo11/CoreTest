8f5cfec5cced0aa0dc1276990c4d2c6a500bbec1
Zikriyojonov <user@WIN-M6UCRNPVE06>
2026-05-15 13:59:15 +0500

    Add criteria classes for querying entities in Contracts
    Introduce BaseCriteria with common filters and pagination. Add Category, Questions, and Test criteria classes inheriting from BaseCriteria. Update namespaces and remove Criteria folder from project file. Minor formatting fix in Test.cs.

---
+++ CoreTest.Contracts/Criteria/Category/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreTest.Contracts.Criteria.Category
+{
+    public class Category : BaseCriteria
+    {
+        public long? Id { get; set; }
+        public string? Text { get; set; }
+    }
+}
