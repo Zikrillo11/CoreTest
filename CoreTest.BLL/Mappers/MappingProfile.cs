da4a11aebfc4789c17be1f6cc181ca1d2c22512e
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-06-08 16:16:21 +0500

    Add AutoMapper, EF Core, and generic repository pattern
    Integrated AutoMapper and EF Core packages across projects. Added MappingProfile for DTO/entity mapping. Implemented generic repository with CRUD operations. Replaced QuestionForShortResult with QuestionForShortResultDTO. Updated project references and cleaned up unused folders.

---
+++ CoreTest.BLL/Mappers/MappingProfile.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using CoreTest.Domain.Entites;
+using CoreTest.Shared.DTOs;
+using CoreTest.Shared.DTOs.Categories;
+using CoreTest.Shared.DTOs.Questions;
+using CoreTest.Shared.DTOs.Tests;
+using CoreTest.Shared.DTOs.TestSolution;
+using CoreTest.Shared.DTOs.Topic;
+using CoreTest.Shared.DTOs.User;
+using CoreTest.Shared.DTOs.UserTestSolutions;
+
+namespace Laptops.BLL.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Category, CategoryForCreateDTO>().ReverseMap();
+
+        CreateMap<Category, CategoryForUpdateDTO>().ReverseMap();
+
+        CreateMap<Category, CategoryForResultDTo>().ReverseMap();
+
+
+        CreateMap<Category, CategoryForShortResultDTO>().ReverseMap();
+
+        CreateMap<Option, OptionForCreateDTO>().ReverseMap();
+
+        CreateMap<Option, OptionForUpdateDTO>().ReverseMap();
+
+        CreateMap<Option, OptionForResultDTO>().ReverseMap();
+
+        CreateMap<Option, OptionForShortResultDTO>().ReverseMap();
+
+
+        CreateMap<Questions, QuestionForCreateDTO>().ReverseMap();
+
+        CreateMap<Questions, QuestionForUpdateDTO>().ReverseMap();
+
+        CreateMap<Questions, QuestionForResultDTO>().ReverseMap();
+
+        CreateMap<Questions, QuestionForShortResultDTO>().ReverseMap();
+
+
+        CreateMap<User, TestForCreateDTO>().ReverseMap();
+
+        CreateMap<User, TestForUpdateDTO>().ReverseMap();
+
+        CreateMap<User, TestForResultDTO>().ReverseMap();
+
+        CreateMap<User, TestForShortResultDTO>().ReverseMap();
+
+
+        CreateMap<TestSolution, TestSolutionForCreateDTO>().ReverseMap();
+
+        CreateMap<TestSolution, TestSolutionForUpdateDTO>().ReverseMap();
+
+        CreateMap<TestSolution, TestSolutionForResultDTO>().ReverseMap();
+
+        CreateMap<TestSolution, TestSolutionForShortResultDTO>().ReverseMap();
+
+
+        CreateMap<Topic, TopicForCreateDTO>().ReverseMap();
+
+        CreateMap<Topic, TopicForUpdateDTO>().ReverseMap();
+
+        CreateMap<Topic, TopicForResultDTO>().ReverseMap();
+
+        CreateMap<Topic, TopicForShortResultDTO>().ReverseMap();
+
+
+        CreateMap<User, UserForCreateDto>().ReverseMap();
+
+        CreateMap<User, UserForUpdateDto>().ReverseMap();
+
+        CreateMap<User, UserForResultDto>().ReverseMap();
+
+        CreateMap<User, UserForShortResultDto>().ReverseMap();
+
+
+        CreateMap<UserTestSolution, UserTestSolutionsForCreateDto>().ReverseMap();
+
+        CreateMap<UserTestSolution, UserTestSolutionsForUpdateDto>().ReverseMap();
+
+        CreateMap<UserTestSolution, UserTestSolutionsForResultDto>().ReverseMap();
+
+        CreateMap<UserTestSolution, UserTestSolutionsForShortResultDto>().ReverseMap();
+    }
+}
