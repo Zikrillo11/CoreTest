dab062f9abb6c0f10078ee5097b2751f6564ad5e
Zikirullo Yoldashev <zikirullo11@outlook.com>
2026-06-08 12:01:48 +0500

    Add user and test solution criteria and service interfaces
    Introduced UserCriteria and UserTestSolution criteria classes for filtering. Added IUserService and IUserTestSolutionService interfaces with CRUD and pagination support using DTOs. These changes support user and test solution management in the application.

---
+++ CoreTest.Contracts/Criteria/User/UserCriteria.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreTest.Contracts.Criteria.User;
+
+public class UserCriteria : BaseCriteria
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Skills { get; set; }
+
+}
---
+++ CoreTest.Contracts/Interface/User/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using CoreTest.Common.Pagination;
+using CoreTest.Contracts.Criteria.User;
+using CoreTest.Shared.DTOs.User;
+
+namespace CoreTest.Contracts.Interface.User;
+
+public interface IUserService
+{
+Task<PagedList<UserForResultDto>> GetAllAsync(UserCriteria criteria);
+    Task<UserForResultDto> GetByIdAsync(long id);
+    Task<long> RegisterAsync(UserForCreateDto newDTO);
+    Task<bool> UpdateAsync(long id, UserForUpdateDto updateDTO);
+    Task<bool> DeleteAsync(long id);
+}
